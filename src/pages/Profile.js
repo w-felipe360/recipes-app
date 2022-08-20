ff059b37c191961958f6e1f706e9d5a8e6ebd889
thspanhol <thales_spanhol@hotmail.com>
2022-08-20 02:30:52 -0300

    profile criado

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,10 +1,65 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
+import Footer from '../components/Footer';
+import Header from '../components/Header';
 
 const Profile = () => {
-  console.log('Profile');
+  const history = useHistory();
+
+  const redirectHistory = (param) => {
+    history.push(param);
+  };
+
+  const redirectLogout = () => {
+    localStorage.clear();
+    history.push('/');
+  };
+
   return (
     <div>
+      <Header />
+      <br />
       Profile
+      {/* Resgatar email do local storage e mostrar nesse h2 */}
+      <h2 data-testid="profile-email">Email</h2>
+      <button
+        type="button"
+        data-testid="profile-done-btn"
+        onClick={
+          () => redirectHistory('/receitas-feitas')
+        }
+      >
+        Done Recipes
+
+      </button>
+      <br />
+      <br />
+      <button
+        type="button"
+        data-testid="profile-favorite-btn"
+        onClick={
+          () => redirectHistory('/receitas-favoritas')
+        }
+      >
+        Favorite Recipes
+
+      </button>
+      <br />
+      <br />
+      <button
+        type="button"
+        data-testid="profile-logout-btn"
+        onClick={
+          () => redirectLogout()
+        }
+      >
+        Logout
+
+      </button>
+      <br />
+      <br />
+      <Footer />
+
     </div>
   );
 };
