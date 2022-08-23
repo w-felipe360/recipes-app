fb814bcebbf536c12eb5dcd8d7e9b423ffe71af2
ThiagoBarbosaDev <thiagobarbosawebdev@gmail.com>
2022-08-23 16:28:26 -0300

    testes

--- src/test/Recipes.test.js
+++ src/test/Recipes.test.js
@@ -1,5 +1,6 @@
 import { screen } from '@testing-library/react';
 import React from 'react';
+import fetch from '../../cypress/mocks/fetch';
 import Recipes from '../pages/Recipes';
 import { renderWithRouterAndRedux } from './renderWithRouterAndRedux';
 
@@ -30,36 +31,24 @@ import { renderWithRouterAndRedux } from './renderWithRouterAndRedux';
 //   },
 // ];
 
-// beforeEach(() => {
-//   jest.spyOn(global, 'fetch')
-//     .mockImplementationOnce(() => Promise.resolve({
-//       json: () => Promise.resolve(mockedData),
-//     }))
-//     .mockImplementationOnce(() => Promise.resolve({
-//       json: () => Promise.resolve(mockedData),
-//     }))
-//     .mockImplementationOnce(() => Promise.resolve({
-//       json: () => Promise.resolve(mockedData),
-//     }))
-//     .mockImplementationOnce(() => Promise.resolve({
-//       json: () => Promise.resolve(mockedData),
-//     }));
-// });
-
+beforeEach(() => {
+  jest.spyOn(global, 'fetch')
+    .mockImplementationOnce(fetch);
+});
 // afterEach(() => {
 //   jest.resetAllMocks();
 // });
 
 describe('Testa o componente Recipes', () => {
   it('Testa se profile icon e page title estão na tela', async () => {
-    const delay = 2000;
     const { history } = renderWithRouterAndRedux(<Recipes />, undefined, '/foods');
-    await new Promise((r) => setTimeout(r, delay));
+    screen.logTestingPlaygroundURL();
     const firstCard = await screen.findByTestId('0-card-img');
+
+    screen.logTestingPlaygroundURL();
     expect(firstCard).toBeInTheDocument();
 
     expect(history.location.pathname).toBe('/foods');
-    // screen.logTestingPlaygroundURL();
   });
   // it('Testa se profile icon e page title estão na tela', async () => {
   //   renderWithRouterAndRedux(<App />, undefined, '/');
