5b5ae285e5681c155afbdd6cbfff24562773ad08
Willian Felipe <w.felipebraz@gmail.com>
2022-08-24 18:30:04 -0300

    feito requisito 11

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,42 +5,38 @@ import profileIcon from '../images/profileIcon.svg';
 import searchIcon from '../images/searchIcon.svg';
 import SearchBar from './SearchBar';
 
-const ICONLESS_PATHNAME = ['/profile', '/done-recipes', '/favorite-recipes'];
+// const ICONLESS_PATHNAME = ['/profile', '/done-recipes', '/favorite-recipes'];
 
 const Header = () => {
   const { location: { pathname } } = useHistory();
-  const hasIcon = () => !ICONLESS_PATHNAME.some((path) => path === pathname);
+  // const hasIcon = () => !ICONLESS_PATHNAME.some((path) => path === pathname);
   const initialState = -1;
   const [click, setClick] = useState(initialState);
 
   return (
     <div>
-      <Link to="/profile">
-        <img
-          src={ profileIcon }
-          alt="profile-icon"
-          data-testid="profile-top-btn"
-        />
-      </Link>
+      <button type="button">
+        <Link to="/profile">
+          <img
+            src={ profileIcon }
+            alt="profile-icon"
+            data-testid="profile-top-btn"
+          />
+        </Link>
+      </button>
 
       <h2 data-testid="page-title">{ capitalizeFirstLetter(pathname) }</h2>
 
-      {
-        hasIcon() ? (
-          <img
-            src={ searchIcon }
-            alt="search-icon"
-            data-testid="search-top-btn"
-          />
-        ) : null
-      }
-
       <button
         type="button"
-        data-testid="search-input"
+        // data-testid="search-input"
         onClick={ () => setClick(click + 1) }
       >
-        Buscar
+        <img
+          src={ searchIcon }
+          alt="profile-icon"
+          data-testid="search-top-btn"
+        />
       </button>
 
       {
---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,107 @@
+import React, { useState } from 'react';
+// import { useDispatch } from 'react-redux';
+// import requestFilteredCategoriesThunk from '../redux/actions/requestFilteredRecipesThunk';
+
+const SearchBar = () => {
+  const [search, setSearch] = useState('');
+  const handleChange = ({ target: { value } }) => {
+    setSearch(value);
+  };
+
+  const [radioSelected, setRadioSelected] = useState('name');
+  const handleRadio = ({ target: { value } }) => {
+    setRadioSelected(value);
+  };
+  const getFood = async () => {
+    let response = '';
+    let data = '';
+    let newData = '';
+    if (radioSelected === 'ingredient') {
+      response = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${search}`);
+      data = await response.json();
+      newData = data.meals[0];
+      console.log(newData);
+    }
+    if (radioSelected === 'name') {
+      response = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${search}`);
+      data = await response.json();
+      newData = data.meals[0];
+      console.log(newData);
+    }
+    if (radioSelected === 'first-letter') {
+      response = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?f=${search}`);
+      data = await response.json();
+      newData = data.meals[0];
+      console.log(newData);
+    }
+    // if(radioSelected === '')
+  };
+
+  const handleClick = () => {
+    if (radioSelected === 'first-letter' && search.length > 1) {
+      global.alert('Your search must have only 1 (one) character');
+    }
+    return getFood();
+    // console.log('input:', search, 'radio:', radioSelected);
+  };
+
+  return (
+    <div className="searchBar">
+      <input
+        data-testid="search-input"
+        value={ search }
+        name="inputName"
+        onChange={ handleChange }
+        type="text"
+      />
+      <div className="radiosSearchBar">
+
+        <label htmlFor="ingredient">
+          <input
+            name="test"
+            value="ingredient"
+            type="radio"
+            id="ingredient"
+            data-testid="ingredient-search-radio"
+            onChange={ handleRadio }
+          />
+          ingredient
+        </label>
+
+        <label htmlFor="name">
+          <input
+            value="name"
+            name="test"
+            type="radio"
+            id="name"
+            data-testid="name-search-radio"
+            onChange={ handleRadio }
+          />
+          Name
+        </label>
+
+        <label htmlFor="firstLetter">
+          <input
+            value="first-letter"
+            name="test"
+            type="radio"
+            id="firstLetter"
+            data-testid="first-letter-search-radio"
+            onChange={ handleRadio }
+          />
+          First letter
+        </label>
+      </div>
+      <div>
+        <button
+          type="button"
+          data-testid="exec-search-btn"
+          onClick={ handleClick }
+        >
+          Procurar
+        </button>
+      </div>
+    </div>
+  );
+};
+export default SearchBar;
