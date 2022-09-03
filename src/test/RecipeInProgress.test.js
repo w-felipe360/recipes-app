6d222fe9ec75c85cb8c085dab6bcc0eb2ee22e3e
ThiagoBarbosaDev <thiagobarbosawebdev@gmail.com>
2022-09-03 10:51:59 -0300

    tests

--- src/test/RecipeInProgress.test.js
+++ src/test/RecipeInProgress.test.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import { screen } from '@testing-library/react';
+import { screen, waitForElementToBeRemoved } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { renderWithRouterAndRedux } from './renderWithRouterAndRedux';
-// import SearchBar from '../components/SearchBar';
 import App from '../App';
 import blackHeartIcon from '../images/blackHeartIcon.svg';
 import whiteHeartIcon from '../images/whiteHeartIcon.svg';
 
 const url = 'foods/52977/in-progress';
-test('se os elementos estão funcionando corretamente', () => {
+test('se os elementos estão funcionando corretamente', async () => {
   const { history } = renderWithRouterAndRedux(<App />);
   history.push(url);
+  await waitForElementToBeRemoved(() => screen.getByText(/Loading.../i));
   const shareIcon = screen.getByTestId('share-btn');
   expect(shareIcon).toBeInTheDocument();
   const heart = screen.getByTestId('favorite-btn');
@@ -49,37 +49,46 @@ test('se os checkboxs estão funcionando corretamente', async () => {
     name: /lentils - 1 cup/i,
   });
   const text = await screen.findByText(/lentils - 1 cup/i);
-  expect(text).toHaveAttribute('class', 'none');
   userEvent.click(checkbox1);
-  expect(text).toHaveAttribute('class', 'line');
+  expect(text).toHaveAttribute('class', 'strikethrough');
 });
 test('se o botão de finalizar receita funciona corretamente', async () => {
   const { history } = renderWithRouterAndRedux(<App />);
+
   history.push('drinks/15997/in-progress');
-  const finishRecipes = await screen.findByText(/finish recipe/i);
-  expect(finishRecipes.disabled).toBe(true);
-  const check1 = await screen.findByRole('checkbox', {
+  await waitForElementToBeRemoved(() => screen.getByText(/Loading.../i));
+  const finishButton = screen.getByTestId('finish-recipe-btn');
+
+  expect(finishButton).toBeDisabled();
+  const check1 = screen.getByRole('checkbox', {
     name: /galliano - 2 1\/2 shots/i,
   });
-  const check2 = await screen.findByRole('checkbox', {
+  userEvent.click(check1);
+  expect(check1).toBeChecked();
+
+  const check2 = screen.getByRole('checkbox', {
     name: /ginger ale/i,
   });
-  const check3 = await screen.findByRole('checkbox', {
+  userEvent.click(check2);
+  expect(check2).toBeChecked();
+
+  const check3 = screen.getByRole('checkbox', {
     name: /ice/i,
   });
-  userEvent.click(check1);
-  userEvent.click(check2);
   userEvent.click(check3);
+  expect(check3).toBeChecked();
+
   const finishRecipes2 = await screen.findByText(/finish recipe/i);
   expect(finishRecipes2).not.toBeDisabled();
   userEvent.click(finishRecipes2);
+
   expect(history.location.pathname).toBe('/done-recipes');
 });
-// test('clipboard', async () => {
-//   const { history } = renderWithRouterAndRedux(<App />);
-//   history.push(url);
-//   const shareIcon = screen.getByTestId('share-btn');
-//   userEvent.click(shareIcon);
-//   const share = await findByText('Link copied!');
-//   expect(share).toBeInTheDocument();
-// });
+test('clipboard', async () => {
+  const { history } = renderWithRouterAndRedux(<App />);
+  history.push(url);
+  const shareIcon = screen.getByTestId('share-btn');
+  userEvent.click(shareIcon);
+  const share = await findByText('Link copied!');
+  expect(share).toBeInTheDocument();
+});
